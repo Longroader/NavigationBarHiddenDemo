d0ea2b7edd06a541ee8a3f7e8699f55a2f8b64f0
longroader <234283063@qq.com>
2016-11-13 23:45:55 +0800

    first commit

---
+++ NavigationBarHiddenDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NavigationBarDemo
+//
+//  Created by LZF on 16/10/24.
+//  Copyright © 2016年 longroader. All rights reserved.
+//  动态隐藏和显示导航条
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UITableViewController
+
+
+@end
+
---
+++ NavigationBarHiddenDemo/ViewController.m
@@ -0,0 +1,66 @@
+//
+//  ViewController.m
+//  NavigationBarDemo
+//
+//  Created by LZF on 16/10/24.
+//  Copyright © 2016年 longroader. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.navigationItem.title = @"上滑隐藏导航条Demo";
+}
+
+/************************** 测试数据 **************************/
+#pragma mark - 单元格数据源方法
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return 100;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString * const ID = @"cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
+    
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:ID];
+    }
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    cell.textLabel.text = [NSString stringWithFormat:@"动态隐藏和显示导航条%ld", indexPath.row];
+    cell.textLabel.textColor = [UIColor whiteColor];
+    cell.backgroundColor = [UIColor purpleColor];
+    return cell;
+}
+/************************** 测试数据 **************************/
+
+#pragma mark 滑动隐藏导航栏
+// 滑动隐藏导航栏
+-(void)scrollViewWillEndDragging:(UIScrollView *)scrollView withVelocity:(CGPoint)velocity targetContentOffset:(inout CGPoint *)targetContentOffse {
+    
+    if(velocity.y > 0) { // 上滑隐藏
+        
+        [self.navigationController setNavigationBarHidden:YES animated:YES];
+        [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
+        
+    } else { // 下滑显示
+        
+        [self.navigationController setNavigationBarHidden:NO animated:YES];
+        [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleDefault];
+    }
+}
+
+@end
